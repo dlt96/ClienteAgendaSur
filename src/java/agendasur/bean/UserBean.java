b401acfa267a72bcc96d94f0f4cdabc196dd0128
Adrian <cardenitas96@gmail.com>
2017-11-14 23:56:38 +0100

    Login correcto

---
+++ src/java/agendasur/bean/UserBean.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agendasur.bean;
+
+import client.AgendaSurService_Service;
+import client.Usuario;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.xml.ws.WebServiceRef;
+
+/**
+ *
+ * @author Adrian
+ */
+@Named(value = "UserBean")
+@SessionScoped
+public class UserBean implements Serializable {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/AgendaSur-war/agendaSurService.wsdl")
+    private AgendaSurService_Service service;
+    
+    private String email;
+    private String password;
+    private Usuario usuario;
+    
+    public UserBean() {
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String doLogin(){
+            if(email!=null){
+                usuario = findUsuario(email);
+            }
+            
+            if(usuario!=null&&usuario.getPassword().equals(password)){
+               return "listEvents";
+            }else{
+                return null;  
+            }
+    }
+    
+    private java.util.List<client.Usuario> findAllUsuario() {
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findAllUsuario();
+    }
+
+    private Usuario findUsuario(java.lang.Object id) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findUsuario(id);
+    }
+}
