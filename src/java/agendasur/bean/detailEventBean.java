0fa4711e2fabec4aed4dbe4da75b531aea8ce4d9
Adrian <cardenitas96@gmail.com>
2017-11-18 00:42:19 +0100

    get Comentarios en el detail event

--- src/java/agendasur/bean/detailEventBean.java
+++ src/java/agendasur/bean/detailEventBean.java
@@ -5,10 +5,14 @@
  */
 package agendasur.bean;
 
+import client.AgendaSurService_Service;
+import client.Comentario;
 import client.Evento;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.xml.ws.WebServiceRef;
 
 /**
  *
@@ -17,6 +21,9 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "detailEventBean")
 @RequestScoped
 public class detailEventBean {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/AgendaSur-war/agendaSurService.wsdl")
+    private AgendaSurService_Service service;
 
     /**
      * Creates a new instance of detailEventBean
@@ -37,6 +44,17 @@ public class detailEventBean {
     public String cargarEvento(Evento event){
         setEvent(event);
         return "Event";
+    }
+    
+    public List<Comentario> getComentarios(){
+        return findCometariosEvento(event);
+    }
+
+    private java.util.List<client.Comentario> findCometariosEvento(client.Evento arg0) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findCometariosEvento(arg0);
     }
     
 }
