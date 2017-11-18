acbe4d63a4d44d30f0419dc398c6f2d346b1f0ed
David Luque <david_92.8@hotmail.com>
2017-11-18 09:55:34 +0000

    Vista Listado + Dropdown Category

--- src/java/agendasur/bean/listadoEventos.java
+++ src/java/agendasur/bean/listadoEventos.java
@@ -7,6 +7,7 @@ package agendasur.bean;
 
 import client.AgendaSurService_Service;
 import client.Evento;
+import client.Tag;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
@@ -25,7 +26,8 @@ public class listadoEventos {
     private AgendaSurService_Service service;
 
     private List<Evento> listaEventos;
-    
+    private List<Tag> listaTags;
+    private String tagSelected;
     /**
      * Creates a new instance of listadoEventos
      */
@@ -35,6 +37,7 @@ public class listadoEventos {
     @PostConstruct
     public void init(){
         listaEventos = findAllEvento();
+        listaTags = findAllTags();
     }
     
     public void setListaEventos(List<Evento> listaEventos) {
@@ -45,11 +48,47 @@ public class listadoEventos {
         return listaEventos;
     }
    
+    public List<Tag> getListaTags() {
+        return listaTags;
+    }
+
+    public void setListaTags(List<Tag> listaTags) {
+        this.listaTags = listaTags;
+    }
+   
+    public String getTagSelected() {
+        return tagSelected;
+    }
+
+    public void setTagSelected(String tagSelected) {
+        this.tagSelected = tagSelected;
+        System.out.println(tagSelected);
+        if(!tagSelected.equals("--")){
+            client.AgendaSurService port = service.getAgendaSurServicePort();
+            Tag t = port.findTag(tagSelected);
+            this.listaEventos =  findEventsByTag(t);
+            System.out.println(this.listaEventos);
+        }else{
+            init();
+        }
+    }
+    
+    public java.util.List<client.Evento> findEventsByTag(Tag tag){
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findTagsEvento(tag);
+    }
     
     public String doBorrar(Evento evento){
             removeEvento(evento);
             init();
         return null;
+    }
+    
+    private java.util.List<client.Tag> findAllTags() {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findAllTag();
     }
 
     private java.util.List<client.Evento> findAllEvento() {
