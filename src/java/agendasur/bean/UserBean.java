6b6b80f67b9aaf1b6abe280e43fdc3088d45c6e2
Adrian <cardenitas96@gmail.com>
2017-11-23 18:08:47 +0100

    Actualiza los tags del usuario

--- src/java/agendasur/bean/UserBean.java
+++ src/java/agendasur/bean/UserBean.java
@@ -35,22 +35,41 @@ public class UserBean implements Serializable {
     private Evento eventoAeditar;
     private double latitude;
     private double longitude;
-    
+
     private List<Evento> listaEventos;
     private List<Tag> listaTags;
+    private String listaUserTagsSelected;
 
-    
-    
     public UserBean() {
     }
-    
-    public void cargarEventosYTags(){
+
+    public String getListaUserTagsSelected() {
+        return listaUserTagsSelected;
+    }
+
+    public void setListaUserTagsSelected(String listaUserTagsSelected) {
+        this.listaUserTagsSelected = listaUserTagsSelected;
+    }
+
+    public void cargarEventosYTags() {
         listaEventos = this.findEventosNoCaducadosYValidados();
     }
-    public void cargarTags(){
+
+    public void cargarTags() {
         listaTags = this.findAllTag();
     }
-    
+
+    public void cargarTagsUsuario() {
+        List<Tag> list = findTagsUsuario(this.usuario.getEmail());
+        if(list.size()>0){
+            listaUserTagsSelected = "";
+            listaUserTagsSelected = list.get(0).getNombre();
+            for(int i = 1; i<list.size();i++){
+                listaUserTagsSelected =listaUserTagsSelected + "," + list.get(i).getNombre();
+            }
+        }
+    }
+
     public List<Tag> getListaTags() {
         return listaTags;
     }
@@ -58,7 +77,7 @@ public class UserBean implements Serializable {
     public void setListaTags(List<Tag> listaTags) {
         this.listaTags = listaTags;
     }
-    
+
     public List<Evento> getListaEventos() {
         return listaEventos;
     }
@@ -129,22 +148,23 @@ public class UserBean implements Serializable {
         if (email != null) {
             usuario = findUsuario(email);
         }
-        
+
         if (usuario != null && usuario.getPassword().equals(password)) {
             this.cargarEventosYTags();
             this.cargarTags();
+            this.cargarTagsUsuario();
             return "listEvents";
         } else {
             return null;
         }
     }
-    
-    public String doLogOut(){
-      FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-       return "LoginJSF";
+
+    public String doLogOut() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "LoginJSF";
     }
-    
-    public String doCrear(){
+
+    public String doCrear() {
         return "crearEvento";
     }
 
@@ -164,7 +184,7 @@ public class UserBean implements Serializable {
     public boolean isMeGusta() {
         return !existeMeGusta(event, usuario);
     }
-    
+
     private Usuario findUsuario(java.lang.Object id) {
         // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
         // If the calling of port operations may lead to race condition some synchronization is required.
@@ -192,5 +212,12 @@ public class UserBean implements Serializable {
         client.AgendaSurService port = service.getAgendaSurServicePort();
         return port.findAllTag();
     }
-    
+
+    private java.util.List<client.Tag> findTagsUsuario(java.lang.String arg0) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        client.AgendaSurService port = service.getAgendaSurServicePort();
+        return port.findTagsUsuario(arg0);
+    }
+
 }
